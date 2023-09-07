25ef9145cd76f8ea8a7b3c32c1efe9f9e1743436
Michael Fuest <ge69fuh@mytum.de>
2023-09-07 10:22:11 +0200

    added multi-dimensional alpha

--- experiments/functions.py
+++ experiments/functions.py
@@ -11,18 +11,19 @@ class FunctionGenerator:
         self.n_samples = n_samples
         self.n_dim = n_dim
 
-    def generate_trigonometric_polynomial_data(self, polynomial_degree=3, alpha=0.5):
+    def generate_trigonometric_polynomial_data(self, polynomial_degree=3):
         # Generate random points in the given range for each dimension
         key = random.PRNGKey(1234)
         x = random.uniform(key, shape=(self.n_samples, self.n_dim))
         y = jnp.zeros(self.n_samples)
         dydx = jnp.array([jnp.zeros(self.n_dim) for _ in range(self.n_samples)])
+        alpha = random.uniform(key, shape=(self.n_dim,))
         gradient = grad(FunctionGenerator.polynomial_and_trigonometric_function)
 
         for i, point in enumerate(x):
-            fx = FunctionGenerator.polynomial_and_trigonometric_function(point, polynomial_degree, alpha)
+            fx = FunctionGenerator.polynomial_and_trigonometric_function(point, alpha, polynomial_degree)
             y = y.at[i].set(fx)
-            dydx = dydx.at[i].set(gradient(point))
+            dydx = dydx.at[i].set(gradient(point, alpha, polynomial_degree))
 
         return x.reshape(-1, self.n_dim), y.reshape(-1, 1), dydx.reshape(-1, self.n_dim)
 
@@ -41,13 +42,16 @@ class FunctionGenerator:
         return x.reshape(-1, self.n_dim), y.reshape(-1, 1), dydx.reshape(-1, self.n_dim)
 
     @staticmethod
-    def polynomial_and_trigonometric_function(x, polynomial_degree=3, alpha=0.5):
+    def polynomial_and_trigonometric_function(x, alpha, polynomial_degree=3):
         return jnp.sum(
-           jnp.array([(1 - alpha) * x[dim] ** (polynomial_degree - dim)
-                      + alpha * jnp.sin(x[dim]) * 10 ** (polynomial_degree - dim)
+           jnp.array([(1 - alpha[dim]) * x[dim] ** (polynomial_degree - dim)
+                      + alpha[dim] * jnp.sin(x[dim]) * 10 ** (polynomial_degree - dim)
                       for dim in range(len(x))])
        )
 
-gen = FunctionGenerator(100, 4)
-x, y, dydx = gen.generate_step_function_data(10)
+gen = FunctionGenerator(100, 3)
+x, y, dydx = gen.generate_trigonometric_polynomial_data(7)
+#plt.scatter(x, y)
+#plt.show()
+
 
